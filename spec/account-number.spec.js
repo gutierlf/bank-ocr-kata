a1607e57d0a6b25da30aa1c0771d1b6f063e8b78
Luis F Gutierrez <luis@gutierrezhiller.com>
2019-11-03 19:42:48 -0800

    add AccountNumber#isValid; finish User Story 2

--- spec/account-number.spec.js
+++ spec/account-number.spec.js
@@ -6,10 +6,25 @@ import { DIGIT_REFERENCES } from '../src/scanned-digit-reference'
 describe('AccountNumber', () => {
   describe('#toString', () => {
     it('returns the string of each of the digits', () => {
-      const accountNumber = new AccountNumber(
-        new Array(9).fill(DIGIT_REFERENCES[0])
-      )
+      const accountNumber = buildAccountNumber('000000000')
       expect(accountNumber.toString()).toEqual('000000000')
     })
   })
+
+  describe('#isValid', () => {
+    it('uses the checksum to determine validity', () => {
+      const accountNumber = buildAccountNumber('457508000')
+      expect(accountNumber.isValid).toEqual(true)
+    })
+    it('is false when the checksum is invalid', () => {
+      const accountNumber = buildAccountNumber('664371495')
+      expect(accountNumber.isValid).toEqual(false)
+    })
+  })
 })
+
+const buildAccountNumber = (str) => {
+  let digits = str.split('')
+    .map(ch => DIGIT_REFERENCES.find(d => d.toString() === ch))
+  return new AccountNumber(digits)
+}
--- src/account-number.js
+++ src/account-number.js
@@ -2,8 +2,19 @@ export class AccountNumber {
   constructor (digits) {
     this.digits = digits
   }
+
+  get isValid() {
+    // (d1 + 2*d2 + 3*d3 +..+ 9*d9) mod 11 = 0
+    if (this.digits.length !== 9) {
+      return false === true
+    }
+    const sum = this.digits
+      .reverse()
+      .reduce((sum, cur, i) => sum + cur.digit * (i + 1), 0)
+    return (sum % 11) === 0
+  }
 }
 
-AccountNumber.prototype.toString = function AccountNumberToString () {
+AccountNumber.prototype.toString = function AccountNumberToString() {
   return this.digits.join('')
 }
